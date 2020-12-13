f2ea2a5c4477075868b169709724f8e2364912c0
johan-avila <johan-avila@outlook.com>
2020-12-12 18:56:45 -0600

    add react router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,7 +13,7 @@ import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return (
@@ -44,4 +44,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
